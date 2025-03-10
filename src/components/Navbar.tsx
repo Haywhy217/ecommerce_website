27ce84414ef8df6d6853ed32da13c198f9dd24c0
Haywhy217 <olanipekunayomide217@gmail.com>
2025-03-10 11:02:00 +0100

    completed mobile menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,32 @@
 import Link from 'next/link'
 import React from 'react'
+import Menu from './Menu'
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
-    <div className='h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64'>
-
-   <Link href="/">AYA</Link>
+    <div className='h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative'>
+      {/* MOBILE */}
+      <div className='flex items-center justify-between h-full'>
+      <Link href="/">
+     <div className='text-2xl tracking-wide'>LuxeLine</div>
+      </Link>
+      <Menu/>
+      </div>
+      {/* BIGGER SCREENS */}
         
+        <div className='hidden md:flex items-center justify-between h-full gap-8'>
+        {/* LEFT */}
+            <div className='w-1/3'>
+            <Link href='/'>
+            <Image src='/logo.png'alt='logo'width={24} height={24}/>
+            <div className='text-2xl tracking-wide'>LuxeLine</div>
+            </Link>
+            
+            </div>
+            {/* RIGHT */}
+            <div className='w-2/3'></div>
+        </div>
     </div>
   )
 }
